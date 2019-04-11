8784e42ed7184d4e32d5c0385a30b98ed4166585
tartarotti <tartarotti.felipe@gmail.com>
2019-04-11 14:52:47 -0300

    sdsd

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+var porta = process.env.PORT || 8080;
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 require('./app/controllers/index')(app);
 
-app.listen(process.env.PORT);
+app.listen(porta);
