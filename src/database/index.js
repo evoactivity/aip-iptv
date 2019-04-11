aa33dfb9013ed0a57252069e83908f42340717b8
tartarotti <tartarotti.felipe@gmail.com>
2019-04-11 15:44:54 -0300

    teste2

--- src/database/index.js
+++ src/database/index.js
@@ -4,6 +4,7 @@ mongoose.set('useFindAndModify', false);
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/noderest', { 
     useNewUrlParser: true, 
      useCreateIndex: true,
+     dbName:"heroku_f6g01j4x"
 });
 
 mongoose.Promise = global.Promise;
