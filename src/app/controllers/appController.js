6ee847f3285c2662b546aa665ec2f3085bf79613
tartarotti <tartarotti.felipe@gmail.com>
2019-05-16 16:19:43 -0300

    req

--- src/app/controllers/appController.js
+++ src/app/controllers/appController.js
@@ -17,7 +17,7 @@ router.post('/',async (req,res)=>{
 
     try{
 
-        console.log(">>>>>>>>>>"+req.body);
+        console.log(req.body);
 
         const device = await Device.findOne({"mac_address": mac_address}, function(err, results){
             if(results){
