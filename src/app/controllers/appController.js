cac9c12e1ffc82bd273c5bb38416fccfeac1d1e5
tartarotti <tartarotti.felipe@gmail.com>
2019-06-03 22:39:45 -0300

    wer

--- src/app/controllers/appController.js
+++ src/app/controllers/appController.js
@@ -12,7 +12,7 @@ router.post('/',async (req,res,next)=>{
     try{
         var body = req.body;
 
-
+        console.log(body.action);
         //player_api.php?username=X&password=X
         //http://psrv.io:80/live/Felipe/vvoYEf9UFn/22886.m3u8
         //http://psrv.io:80/player_api.php?username=Felipe&password=vvoYEf9UFn&action=get_short_epg&stream_id=18301
