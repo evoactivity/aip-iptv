654d8851459aebad51db5de8263b3117cfefcdfa
tartarotti <tartarotti.felipe@gmail.com>
2019-06-16 19:18:45 -0300

    cors

--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middlewares/auth');
 const Device = require('../models/device');
+var cors = require('cors'); app.use(cors());
 
 router.use(authMiddleware);
 
