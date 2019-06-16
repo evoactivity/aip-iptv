dc9c1530d51f5b185cbe65f118caf91587da41ae
tartarotti <tartarotti.felipe@gmail.com>
2019-06-16 20:57:01 -0300

    qwesad

--- src/app/controllers/panelController.js
+++ src/app/controllers/panelController.js
@@ -12,15 +12,18 @@ router.post('/',async (req,res,next)=>{
         var body = req.body;
         console.log(body);
         var url;
+        var third_server_login = false;
 
         await Device.findOne({"third_server_login": body.username}, function(err, results){
             if(results){                        
                 console.log('Encontrou third_server_login');
                 url = results.url;
+                third_server_login = true;
             }
             else
             {
                 console.log('NÃO Encontrou third_server_login');
+                third_server_login = false;
             }
         });    
         
@@ -28,15 +31,17 @@ router.post('/',async (req,res,next)=>{
       
         // url = "http://psrv.io:80/panel_api.php?username=Felipe&password=vvoYEf9UFn";
 
+        if(third_server_login){
 
-        iptv.getPanelApi(url).then((result) => {
-            console.log("Resultado getPanelApi appcontrolle ->");
-            return res.send(result);  
-        })
-        .catch((error) => {
-            console.log("Catch appcontrolle ->" + error );
-            return  next(error);
-        });  
+            iptv.getPanelApi(url).then((result) => {
+                console.log("Resultado getPanelApi appcontrolle ->");
+                return res.send(result);  
+            })
+            .catch((error) => {
+                console.log("Catch appcontrolle ->" + error );
+                return  next(error);
+            });  
+        }
      
              
         
